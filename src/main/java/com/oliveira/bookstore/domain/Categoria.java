f0d9d49c82b77dd34ac70d7fb9271467e4537d76
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-07-15 08:59:36 -0300

    Finalizando as classes de domínio

--- src/main/java/com/oliveira/bookstore/domain/Categoria.java
+++ src/main/java/com/oliveira/bookstore/domain/Categoria.java
@@ -1,8 +1,14 @@
 package com.oliveira.bookstore.domain;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,14 +16,20 @@ import lombok.Setter;
 @Getter
 @Setter 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Categoria {
-	
+@Entity
+public class Categoria implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
 	@EqualsAndHashCode.Include
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String nome;
 	private String descricao;
 	
+	@OneToMany(mappedBy = "categoria")
 	private List<Livro> livros = new ArrayList<>();
 
 	public Categoria() {
--- src/main/java/com/oliveira/bookstore/domain/Livro.java
+++ src/main/java/com/oliveira/bookstore/domain/Livro.java
@@ -1,5 +1,13 @@
 package com.oliveira.bookstore.domain;
 
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,15 +15,22 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Livro {
+@Entity
+public class Livro implements Serializable{
 	
+	private static final long serialVersionUID = 1L;
+
 	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	private String titulo;
 	private String nomeAutor;
 	private String texto;
 	
+	@ManyToOne
+	@JoinColumn(name = "idcategoria")
 	private Categoria categoria;
 
 	public Livro() {
