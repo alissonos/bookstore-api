3b6e6f7e3f63be4a8e3f1d1c5fb33ebda106dce5
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-07-18 21:07:08 -0300

    findAll para Categoria utilizando padrão DTO

---
+++ src/main/java/com/oliveira/bookstore/dtos/CategoriaDTO.java
@@ -0,0 +1,28 @@
+package com.oliveira.bookstore.dtos;
+
+import java.io.Serializable;
+
+import com.oliveira.bookstore.domain.Categoria;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class CategoriaDTO implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String nome;
+	private String descricao;
+	
+	public CategoriaDTO(Categoria obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.descricao = obj.getDescricao();
+	}
+	
+	
+}
