bd8aed0e9a6be2695da06b290f7e247572cff170
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-07-25 15:43:20 -0300

    Delete para Categoria

--- src/main/java/com/oliveira/bookstore/service/CategoriaService.java
+++ src/main/java/com/oliveira/bookstore/service/CategoriaService.java
@@ -40,4 +40,9 @@ public class CategoriaService {
 		obj.setDescricao(objDto.getDescricao());
 		return categoriaRepository.save(obj);
 	}
+
+	public void delete(Integer id) {
+		findById(id);
+		categoriaRepository.deleteById(id);
+	}
 }
