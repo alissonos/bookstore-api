31121a73b15187a2a6b8034044fedb99da1c3d35
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-07-27 19:43:26 -0300

    DELETE para Livros

--- src/main/java/com/oliveira/bookstore/service/LivroService.java
+++ src/main/java/com/oliveira/bookstore/service/LivroService.java
@@ -52,5 +52,10 @@ public class LivroService {
 		return livroRepository.save(obj);
 		
 	}
+
+	public void delete(Integer id) {
+		Livro obj = findById(id);
+		livroRepository.delete(obj);
+	}
 	
 }
